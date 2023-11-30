867242f4d23a368b028e7eeb272cb7184de702cc
John <johnmoreau19@gmail.com>
2023-11-29 16:34:53 -0800

    Add project files.

---
+++ CSS233_M9_PigGame_JohnMoreau/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSS233_M9_PigGame_JohnMoreau.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
